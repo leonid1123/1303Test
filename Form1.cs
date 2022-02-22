c2d0c9e0b7e1e22de132ac988c95e07ee612b07c
Leo <47742726+leonid1123@users.noreply.github.com>
2022-02-22 17:21:13 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1303Test
+{
+    public partial class Form1 : Form
+    {
+        public float sum = 0f;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name;
+            int quantity;
+            int price;
+
+            name = textBox1.Text.Trim();
+            quantity = int.Parse(textBox2.Text);
+            price = int.Parse(textBox3.Text);
+            sum += price * quantity;
+
+
+            listBox1.Items.Add(name +" "+ quantity.ToString()+"шт. "+price.ToString()+"руб.");
+            label4.Text = "Итоговая стоимость: " + sum.ToString();
+        }
+    }
+}
